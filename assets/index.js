f20b370c3330894f8c5180d5a7bc61eda6654d6f
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-05-07 22:01:38 +0100

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -3,14 +3,12 @@ window.addEventListener(
     async () => {
         const text = document.getElementById( "comingSoon" );
         text.innerText = "Making the next big thing,";
-        setTimeout(() => text.className = "disappear comingSoon", 1 * 1000);
-        await new Promise((res) => setTimeout(() => res(), 2.5 * 1000));
+        setTimeout(() => text.className = "disappear comingSoon", 2 * 1000);
+        await new Promise((res) => setTimeout(() => res(), 3 * 1000));
         text.className = "comingSoon appear";
         text.innerText = "Together!";
-        setTimeout(() => {
-            text.className = "disappear comingSoon"
-        }, 1 * 1000);
-        await new Promise((res) => setTimeout(() => res(), 2.5 * 1000));
+        setTimeout(() => text.className = "disappear comingSoon", 2 * 1000);
+        await new Promise((res) => setTimeout(() => res(), 3 * 1000));
         document.getElementById( "comingSoon" ).remove();
 
         const comingSoon = document.getElementById( "comingSoonElement" );
@@ -18,7 +16,7 @@ window.addEventListener(
         comingSoon.className = "comingSoonElement appear";
 
         const socials = document.getElementById( "socials" );
-        await new Promise((res) => setTimeout(() => res(), 1 * 1000));
+        await new Promise((res) => setTimeout(() => res(), 1.5 * 1000));
         socials.style = null;
         socials.className = "comingSoonElement appear";
     },
