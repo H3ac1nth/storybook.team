f47d1adea2aa4ad8cb884b6d2c00ae76b83e5761
xKingDark <47607185+DarkGamerYT@users.noreply.github.com>
2023-05-07 21:49:33 +0100

    Add files via upload

--- assets/index.js
+++ assets/index.js
@@ -7,9 +7,19 @@ window.addEventListener(
         await new Promise((res) => setTimeout(() => res(), 2.5 * 1000));
         text.className = "comingSoon appear";
         text.innerText = "Together!";
-        setTimeout(() => text.className = "disappear comingSoon", 1 * 1000);
+        setTimeout(() => {
+            text.className = "disappear comingSoon"
+        }, 1 * 1000);
         await new Promise((res) => setTimeout(() => res(), 2.5 * 1000));
-        text.className = "comingSoon appear";
-        text.innerText = "Coming soon.";
+        document.getElementById( "comingSoon" ).remove();
+
+        const comingSoon = document.getElementById( "comingSoonElement" );
+        comingSoon.style = null;
+        comingSoon.className = "comingSoonElement appear";
+
+        const socials = document.getElementById( "socials" );
+        await new Promise((res) => setTimeout(() => res(), 1 * 1000));
+        socials.style = null;
+        socials.className = "comingSoonElement appear";
     },
 );
